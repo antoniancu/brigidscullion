e738d6fae25e498fafb0fc12a69c047b591b2379
Olha Yanishevska <olhayanishevska@hotmail.com>
2017-02-27 16:33:42 -0800

    replaced more content with full content

--- wp-content/themes/brigid/template_home.php
+++ wp-content/themes/brigid/template_home.php
@@ -21,32 +21,7 @@ get_header(); ?>
                     </div>
                     <?php } ?>
                 <div class="pr-content">
-                  <?php  $morecon = get_the_content_with_formatting();
-                  $span_id = get_the_id();
-                  $span_id = "more-".$span_id;
-                  $after_more = explode( '<span id="'.$span_id.'"></span>', $morecon ); ?>
-                    <div class="articles-new-first">
-				    <?php if( $after_more[0] ) {
-					echo $content = $after_more[0];
-				    } ?>
-					<?php  if( $after_more[1] ) { ?>
-				   <a href="javascript:void();" id="readmore" class="read_more">Read More ></a>
-						<?php } ?>
-
-				</div>
-				<div class="articles-new-second">
-				 <?php  if( $after_more[1] ) {
-					echo $content = $after_more[1];
-				    }
-					
-				?>
-				  <br><a href="javascript:void();" id="less" class="read_more">< Less</a>			
-				
-                </div>
-             
-                <?php if(get_field('contact_button_text')){ ?>
-                    <a class="contact-btn" href="<?php if(get_field('contact_button_url')){echo get_field('contact_button_url');} ?>" target="_blank"><?php echo get_field('contact_button_text'); ?></a>
-                <?php } ?>
+                    <?php the_content(); ?>
                 </div>
 				 
                     <?php  endwhile;
