e54fb6584e68484446e4c5f4699565aa9851dda2
Olha Yanishevska <olhayanishevska@hotmail.com>
2017-03-28 17:27:23 -0400

    buyer properties changes

--- wp-content/themes/brigid/template_home.php
+++ wp-content/themes/brigid/template_home.php
@@ -30,7 +30,6 @@ get_header(); ?>
             </div>
         </div>
 
-
         <div class="sold-property-section">
             <div class="container">
                 <div class="row">
@@ -77,48 +76,40 @@ get_header(); ?>
         <div class="container">
             <?php echo get_field('for_buyers'); ?>
         </div>
-
+        <?php if( have_rows('featured_buyer_properties') ): ?>
         <div class="buyer-property-section">
             <div class="container">
-                <div class="row">
+                <?php while ( have_rows('featured_buyer_properties') ) : the_row(); ?>
                     <?php
-                    if( have_rows('featured_buyer_properties') ):
-                        while ( have_rows('featured_buyer_properties') ) : the_row();
-                            $buyer_property_post = get_sub_field('buyer_property');
-                            $featured_buyer_title = esc_html(get_sub_field('title'));
-                            if( $featured_buyer_title || $buyer_property_post ) {
-                                ?>
-                                <!--                                --><?php //var_dump(get_sub_field('buyer_property'));?>
+                        $featured_buyer_title = esc_html(get_sub_field('title'));
+                        $buyer_property_post = get_sub_field('buyer_property'); //contains the ID of the post grabbed from the ACF repeater field
+                        $buyer_image_home_acf = esc_url( get_sub_field('buyer_property_acf_image')['sizes']['property_image'] );
+                        $buyer_image_from_post = esc_url( wp_get_attachment_image_src( get_post_thumbnail_id( $buyer_property_post->ID ), 'buyer_property_image')[0] );
+                        if( $buyer_property_post && ( $buyer_image_home_acf || $buyer_image_from_post ) )  :
+                            $valid_properties_exist += 1;
+                            ?>
+                            <div class="row">
                                 <div class="col-sm-4">
-                                    <a href="<?php echo get_permalink( get_sub_field('buyer_property') );?>">
+                                    <a href="<?php echo get_permalink( $buyer_property_post );?>">
                                         <div class="buyer-property-inner">
                                             <div class="heading">
-                                                <?php echo esc_html( get_sub_field('title') ); ?>
+                                                <?php echo ( ! empty($featured_buyer_title ) ? $featured_buyer_title : $buyer_property_post->post_title ); ?>
                                             </div>
-                                            <?php  $buyer_image = wp_get_attachment_image_src( get_post_thumbnail_id
-                                            ( $buyer_property_post->ID ), 'buyer_property_image'); ?>
-                                            <img src="<?php echo esc_url($buyer_image[0]); ?>" class="property-thumbnail">
-                                            <div class="address"><?php echo get_the_title( get_sub_field
-                                                ('buyer_property') );
-                                                ?>
+                                            <img src="<?php echo ( ! empty($buyer_image_home_acf) ? $buyer_image_home_acf : $buyer_property_post ); ?>" class="property-thumbnail">
+                                            <div class="address">
+                                                <?php echo get_the_title( get_sub_field('buyer_property') ); ?>
                                                 <br>Read their story here..
                                             </div>
                                         </div>
                                     </a>
                                 </div>
-                                <?php
-                            }
-                        endwhile;
-                    else :
-                        // no rows found
-                    endif;
-                    ?>
-                </div>
+                            </div>
+                        <?php endif; ?>
+                <?php endwhile; ?>
             </div>
         </div>
+        <?php endif; ?>
     </section>
-
-
 
 <?php get_footer();
 ?>
